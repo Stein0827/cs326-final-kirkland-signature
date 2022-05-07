1fdc4f822ff99a7f4029f281a33c543b1f395ad4
Suyash-Deshmukh <89959627+Suyash-Deshmukh@users.noreply.github.com>
2022-05-07 18:11:15 -0400

    cleaning up + finalizing all CRUD

--- server/mongodb.js
+++ server/mongodb.js
@@ -12,10 +12,6 @@ export class MapDatabase {
       serverApi: ServerApiVersion.v1,
     });
 
-    // Get the database.
-    // this.users = this.client.db('users');
-    // this.events = this.client.db('events');
-
     this.db = this.client.db('UMAP-database');
 
     // Init the database.
@@ -38,47 +34,34 @@ export class MapDatabase {
 
   // CREATE a user in the database.
   async createUser(name, email, password) {
-    // const new_user = {
-    //   user_name : name,
-    //   user_email : email,
-    //   password : password,
-    //   events : []
-    // };
     let collision = await this.users.find({'user_email': email}).toArray();
     if (collision.length > 0) {
       return false;
     }
     const res = await this.users.insertOne({user_name: name, user_email: email, password: password, events: []});
-    // Note: the result received back from MongoDB does not contain the
-    // entire document that was inserted into the database. Instead, it
-    // only contains the _id of the document (and an acknowledged field).
     return res;
   }
 
-  async testEvent(){
-    const user = await this.createUser("BOB", "BOB@BOB.com", "BOB");
-    const res = await this.events.insertOne({ host_id: user.insertedId,  host_name : "user.user_name", event_name: "THIS WILL BE DELETED", event_desc : 'event.event_ desc',
-    event_location : [-72.52628581400859,42.38891007248816], event_time : 'event.event_time', attendees : "event.attendees"});
-    return [res, user];
-  }
+  // async testEvent(){
+  //   const user = await this.createUser("BOB", "BOB@BOB.com", "BOB");
+  //   const res = await this.createEvent({ host_id: user.insertedId,  host_name : "user.user_name", event_name: "THIS WILL BE DELETED", event_desc : 'event.event_ desc',
+  //   event_location : [-72.52628581400859,42.38891007248816], event_time : 'event.event_time', attendees : "event.attendees"});
+  //   return [res, user];
+  // }
 
     // CREATE a user in the database.
   async createEvent(event) {
 
     let user = await this.readUser(event.host_id);
     let collision = await this.events.find({'event_name': event.event_name}).toArray();
-    console.log("HIII");
     if (collision.length > 0) {
       return false;
     }
-    const res = await this.events.insertOne({'_id': ObjectId(id), 'host_id': event.host_id,  'host_name' : user.user_name, 'event_name': event.event_name, event_desc : 'event.event_ desc',
+    const res = await this.events.insertOne({'host_id': event.host_id,  'host_name' : user[0].user_name, 'event_name': event.event_name, event_desc : 'event.event_ desc',
     'event_location' : event.event_location, 'event_time' : event.event_time, 'attendees' : event.attendees});
     
-    user.events.push(res);
+    user[0].events.push(res);
     await this.updateUser(user);
-    // Note: the result received back from MongoDB does not contain the
-    // entire document that was inserted into the database. Instead, it
-    // only contains the _id of the document (and an acknowledged field).
     return res;
   }
 
@@ -147,20 +130,13 @@ export class MapDatabase {
     return res;
   }
 
-  // DELETE a user from the database.
   async deleteUser(id) {
-    // Note: the result received back from MongoDB does not contain the
-    // entire document that was deleted from the database. Instead, it
-    // only contains the 'deletedCount' (and an acknowledged field).
     const res = await this.users.deleteOne({ _id: id });
     return res;
   }
 
-    // DELETE an event from the database.
+
   async deleteEvent(id) {
-    // Note: the result received back from MongoDB does not contain the
-    // entire document that was deleted from the database. Instead, it
-    // only contains the 'deletedCount' (and an acknowledged field).
     const res = await this.events.deleteOne({ _id: id });
     return res;
   }
